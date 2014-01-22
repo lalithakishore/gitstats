e5141a9ec558c7acfd57be9db8ff3d4d5a9f39eb
lalithakishore <lalithkishore@gmail.com>
2014-01-22 22:09:01 +0530

    Update gitstats.R

--- R/gitstats.R
+++ R/gitstats.R
@@ -10,41 +10,7 @@
 #' @export
 
 
-pow2 <- function(n=3,m=2){
-  return(pow(n,m))
-}
 
 gitstats <- function (id = "hadley", type = c("users", "orgs"), max=20) {
-	type <- match.arg(type, choices=c('users','orgs'))
-	
-	#call github API using httr
-	url2 <- paste("https://api.github.com", type, id, "repos?per_page=200", sep="/")
-	xx <- GET(url2)
-	if(xx$status != 200){
-		stop("Github returned an error: ", xx$status)
-	}
-	tt <- fromJSON(rawToChar(xx$content))
-	
-	#convert to data frame
-	out <- do.call("rbind", lapply(tt, "[", c("name", "forks", "watchers")))
-	out <- as.data.frame(lapply(as.data.frame(out), unlist))
-	
-	#resort factor)
-	out <- out[head(order(out$watchers, decreasing=TRUE), max),];
-	out$name <- factor(out$name, levels=rev(out$name));
-	
-	#reshape to "long" dataframe"
-	names(out) <- c("Repo", "Forks", "Stars")
-	out2 <- reshape2::melt(out, id = 1)
-	
-	#create ggplot object
-	myplot <- ggplot(out2, aes(Repo, value)) + 
-    geom_bar(stat="identity") + coord_flip() + 
-		facet_wrap(~variable) + 
-    theme_bw(base_size = 18) + 
-    xlab("") + ylab("")
-	
-	#don't return anything
-	print(myplot)  
-	invisible();
+	return(10);
 }
